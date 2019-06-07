b9e93b96e180e26b36ba98fb80b190c3a4585df4
John Reese <john@reese.dev>
2019-06-07 00:16:03 -0400

    Add sp role limits

--- azure/modules/service-principal/outputs.tf
+++ azure/modules/service-principal/outputs.tf
@@ -10,3 +10,7 @@ output "service_principal_client_secret" {
   sensitive = true
   value     = "${azuread_service_principal_password.service_principal.value}"
 }
+
+output "service_principal_role" {
+  value = "service_principal_role"
+}
--- azure/modules/service-principal/service-principal.tf
+++ azure/modules/service-principal/service-principal.tf
@@ -32,3 +32,45 @@ resource "azuread_service_principal_password" "service_principal" {
     command = "sleep 30"
   }
 }
+
+data "azurerm_subscription" "current" {}
+
+resource "azurerm_role_definition" "service_principal_role" {
+  name        = "service_principal_role"
+  scope       = "${data.azurerm_subscription.current.id}"
+  description = "This role provides the required permissions needed by Kubernetes to: Manager VMs, Routing rules, Mount azure files and Read container repositories"
+
+  permissions {
+    actions = [
+      "Microsoft.Compute/virtualMachines/read",
+      "Microsoft.Compute/virtualMachines/write",
+      "Microsoft.Compute/disks/write",
+      "Microsoft.Compute/disks/read",
+      "Microsoft.Network/loadBalancers/write",
+      "Microsoft.Network/loadBalancers/read",
+      "Microsoft.Network/routeTables/read",
+      "Microsoft.Network/routeTables/routes/read",
+      "Microsoft.Network/routeTables/routes/write",
+      "Microsoft.Network/routeTables/routes/delete",
+      "Microsoft.Storage/storageAccounts/fileServices/fileShare/read",
+      "Microsoft.ContainerRegistry/registries/read",
+      "Microsoft.Network/publicIPAddresses/read",
+      "Microsoft.Network/publicIPAddresses/write",
+    ]
+
+    not_actions = [
+      "Microsoft.Compute/virtualMachines/*/action",
+      "Microsoft.Compute/virtualMachines/extensions/*",
+    ]
+  }
+
+  assignable_scopes = [
+    "${data.azurerm_subscription.current.id}",
+  ]
+}
+
+resource "azurerm_role_assignment" "service_principal_role_assignment" {
+  scope                = "${module.azurerm_resource_group.resource_group.id}"
+  role_definition_name = "${azurerm_role_definition.service_principal_role.name}"
+  principal_id         = "${azuread_service_principal.service_principal.id}"
+}
