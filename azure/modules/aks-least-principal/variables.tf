9a913ae5e0724c7348bd1291cdf17a3565c26080
John Reese <john@reese.dev>
2019-06-24 22:37:10 -0400

    add variables

--- azure/modules/aks-least-principal/variables.tf
+++ azure/modules/aks-least-principal/variables.tf
@@ -1,3 +1,24 @@
+variable "prefix" {
+  description = "The prefix for the resources created in the specified Azure Resource Group"
+  default     = "cluster"
+}
+
+variable "location" {
+  default     = "eastus"
+  description = "The location for the AKS deployment"
+}
+
 variable "service_principal_name" {
-  description = "The name of the service principal to create for the AKS cluster."
+  description = "The name of the service principal to create for the AKS cluster"
+  default     = "aks_service_principal"
+}
+
+variable "agents_count" {
+  description = "The number of Agents that should exist in the Agent Pool"
+  default     = 2
+}
+
+variable "agents_size" {
+  default     = "Standard_DS2_v2"
+  description = "The default virtual machine size for the Kubernetes agents"
 }
